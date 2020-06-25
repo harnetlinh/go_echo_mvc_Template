6da94c2d5d1fb81a992cc853df6d159f131432de
linh.hngoc <linh.hngoc@anlab.pro>
2020-06-25 11:21:48 +0700

    Create folder with server.go

---
+++ models/server.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/labstack/echo"
+	"github.com/labstack/echo/middleware"
+)
+
+func main() {
+	e := echo.New()
+	e.Use(middleware.Logger())
+	e.Use(middleware.Recover())
+
+	e.Use(middleware.CORSWithConfig(middleware.CORSConfig{
+		AllowOrigins: []string{"*"},
+		AllowMethods: []string{echo.GET, echo.PUT, echo.POST, echo.DELETE},
+	}))
+
+	e.GET("/", func(c echo.Context) error {
+		return c.JSON(http.StatusCreated, "Welcome mvc echo with mysql app using Golang")
+	})
+
+	e.Logger.Fatal(e.Start(":8081")) // start with log
+	// e.Start(":8081")
+}
